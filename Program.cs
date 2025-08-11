0abf093cc36a46f97d20a5a1a5265de4eb8fc590
Rebek <160554014+Tesfild@users.noreply.github.com>
2025-08-11 16:12:06 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia("1199999999", "Nokia Tijolão", "123456789", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Smartphone iphone = new Iphone("1198888888", "iPhone 14 Pro", "987654321", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
+
+Console.ReadLine();
